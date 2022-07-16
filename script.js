8c975b13947be138660b4d11d84d4ead7d7974f6
MokS49 <vladislavletichevsky@gmail.com>
2022-07-16 16:43:05 +0200

    function changeColor

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const button = document.querySelector('.button')
+const background = document.querySelector('.wrapper')
+
+function changeColor1() {
+    background.style.background = "#aa00ff"
+    button.onclick = () => {
+        changeColor2()
+    }
+}
+
+function changeColor2() {
+    background.style.background = "yellow"
+    button.onclick = () => {
+        changeColor1()
+    }
+}
 
 button.onclick = () => {
-    alert('Ok!')
+    changeColor1()
 }
