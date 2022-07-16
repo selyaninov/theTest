4c9db7187b3e4c547d76b426be33b9d3fc1dead3
selyaninov <nikitaskin46@gmail.com>
2022-07-16 18:22:59 +0300

    Добавление анимации на первом экране

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function changeColor2() {
 button.onclick = () => {
     changeColor1()
 }
+
+button.addEventListener("click", function(e) {
+    animateButton(button);
+});
+
+function animateButton(element) {
+    element.classList.add('animate__animated', 'animate__flip');
+    element.addEventListener('animationend', () => {
+        element.classList.remove('animate__animated', 'animate__flip');
+    });
+}
